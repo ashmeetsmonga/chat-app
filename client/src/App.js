51a9a40831377541d91b6e2abbc1fc1ca6bed399
ashmeetsmonga <ashmeetsmonga@gmail.com>
2022-12-26 19:55:19 +0530

    added uuid

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Button } from "@material-ui/core";
 import Login from "./components/Login";
 
 function App() {
-	return <Login />;
+	const [id, setId] = useState("");
+
+	console.log(id);
+
+	return <Login onIdSubmit={setId} />;
 }
 
 export default App;
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "./Login.css";
 import { Button, TextField } from "@material-ui/core";
+import { useRef } from "react";
+import { v4 as uuidV4 } from "uuid";
 
-const Login = () => {
+const Login = ({ onIdSubmit }) => {
+	const inputRef = useRef();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		onIdSubmit(inputRef.current.value);
+	};
+
+	const createNewId = () => onIdSubmit(uuidV4());
+
 	return (
 		<div className='login-page'>
-			<form className='login-form' action=''>
-				<TextField variant='outlined' label='ID' name='userId' />
+			<form className='login-form'>
+				<TextField inputRef={inputRef} variant='outlined' label='ID' name='userId' />
 				<div className='button-group'>
-					<Button variant='contained' color='primary'>
+					<Button type='submit' variant='contained' color='primary' onClick={handleSubmit}>
 						Login
 					</Button>
-					<Button variant='contained' color='secondary'>
+					<Button variant='contained' color='secondary' onClick={createNewId}>
 						Create New Id
 					</Button>
 				</div>
